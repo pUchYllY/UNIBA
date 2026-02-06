54ed136a44115f161da8c1274f787feeabe099e7
SL0rD <antoniopacilli.info@gmail.com>
2026-02-06 14:53:34 +0000

    Aggiunto diagramma di classe concettuale e diagramma di classe a prospettiva software in Mermaid. Creati 4 Moduli Java. Iniziata implementazione della progettazione al modulo Calcolatrice.java (Main class)

--- Metodi Avanzati Programmazione/Calcolatrice/src/main/java/com/mycompany/calcolatrice/Calcolatrice.java
+++ Metodi Avanzati Programmazione/Calcolatrice/src/main/java/com/mycompany/calcolatrice/Calcolatrice.java
@@ -1,16 +1,38 @@
-/*
+ /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
+import java.util.*
+import java.io.*
+
 package com.mycompany.calcolatrice;
+import com.mycompany.calcolatrice.util.Console
+import com.mycompany.calcolatrice.util.Operazioni
+import com.mycompany.calcolatrice.util.Memoria
+//import Operazioni.java
 
 /**
  *
  * @author antonio
  */
-public class Calcolatrice {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
+public class Calcolatrice {
+	 public void leggiOperazione(BufferedReader br){
+		 br=new BufferedReader(new InputStreamReader(System.in)); // modo piu' efficiente di new Scanner(System.in)
+	 }
+	 public static void main(String[] args) {
+		 BufferedReader br;
+		 String operazione;
+		 String risposta;
+
+		 System.out.println("----------------- CALCOLATRICE -----------------");
+		 do{
+			 System.out.print("Inserire il calcolo da eseguire: ");
+
+			 leggiOperazione(br);
+			 operazione=br.readLine();
+
+			 //console.effettuaOperazione(operazione);
+		 }while(risposta!="no")
+	 }
+ }
---
+++ Metodi Avanzati Programmazione/Calcolatrice/src/main/java/com/mycompany/calcolatrice/Operazioni.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calcolatrice;
+
+/**
+ *
+ * @author antonio
+ */
+
+public class Operazioni{
+	public static double addizione(){
+		double a,b;
+
+		s
+	}
+}
