816eb4d20bf6e2c41f010a3cbc24626cc1d0a29b
pUchYllY <antoniopacilli.info@gmail.com>
2026-02-09 17:04:25 +0100

    Sistemata sintattica nel modulo Calcolatrice.java

--- Metodi Avanzati Programmazione/Calcolatrice/src/main/java/com/mycompany/calcolatrice/Calcolatrice.java
+++ Metodi Avanzati Programmazione/Calcolatrice/src/main/java/com/mycompany/calcolatrice/Calcolatrice.java
@@ -1,15 +1,14 @@
- /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+/*
+* Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+*/
 
-import java.util.*
-import java.io.*
+import java.util.*;
+import java.io.*;
 
 package com.mycompany.calcolatrice;
-import com.mycompany.calcolatrice.util.Console
-import com.mycompany.calcolatrice.util.Operazioni
-import com.mycompany.calcolatrice.util.Memoria
-//import Operazioni.java
+import com.mycompany.calcolatrice.util.Console;
+import com.mycompany.calcolatrice.util.Operazioni;
+import com.mycompany.calcolatrice.util.Memoria;
 
 /**
  *
@@ -17,22 +16,23 @@ import com.mycompany.calcolatrice.util.Memoria
  */
 
 public class Calcolatrice {
-	 public void leggiOperazione(BufferedReader br){
-		 br=new BufferedReader(new InputStreamReader(System.in)); // modo piu' efficiente di new Scanner(System.in)
-	 }
-	 public static void main(String[] args) {
-		 BufferedReader br;
-		 String operazione;
-		 String risposta;
+	public static void leggiOperazione(BufferedReader br) {
+		br = new BufferedReader(new InputStreamReader(System.in)); // modo piu' efficiente di new Scanner(System.in)
+	}
 
-		 System.out.println("----------------- CALCOLATRICE -----------------");
-		 do{
-			 System.out.print("Inserire il calcolo da eseguire: ");
+	public static void main(String[] args) {
+		BufferedReader br;
+		String operazione;
+		String risposta;
 
-			 leggiOperazione(br);
-			 operazione=br.readLine();
+		System.out.println("----------------- CALCOLATRICE -----------------");
+		do {
+			System.out.print("Inserire il calcolo da eseguire: ");
 
-			 //console.effettuaOperazione(operazione);
-		 }while(risposta!="no")
-	 }
- }
+			leggiOperazione(br);
+			operazione = br.readLine();
+
+			// console.effettuaOperazione(operazione);
+		} while (risposta != "no");
+	}
+}
